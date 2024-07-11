c4239e9034b1d57d2c2f7238093420d58c54e1b0
Your Name <manhhung.minhchuan@gmail,com>
2024-07-11 20:02:19 +0700

    fix header

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,3 +1,7 @@
 function showSearch() {
     document.querySelector('#search').classList.toggle('active-search');
 }
+
+function showSearchRps() {
+    document.querySelector('#search-rps').classList.toggle('active-search');
+}
