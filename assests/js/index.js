4e5795bbfe64bc1d1d655732920eca490fc6f969
manhhung <manhhung.minhchuan@gmail.com>
2024-07-15 15:33:06 +0700

    responsive

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,7 +1,22 @@
 function showSearch() {
-    document.querySelector('#search').classList.toggle('active-search');
+  document.querySelector('#search').classList.toggle('active-search');
 }
 
 function showSearchRps() {
-    document.querySelector('#search-rps').classList.toggle('active-search');
+  document.querySelector('#search-rps').classList.toggle('active-search');
 }
+
+function showNav() {
+  document.querySelector('.main-menu').classList.toggle('active-search');
+}
+
+const menuItems = document.querySelectorAll('li');
+
+menuItems.forEach(item => {
+  const submenu = item.querySelector('.drop-item');
+  item.addEventListener('click', () => {
+    submenu.classList.toggle('active-search');
+  });
+});
+
+
